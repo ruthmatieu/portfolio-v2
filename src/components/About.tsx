1270b296836f72c527b3f6b87ff5d3b229d6c527
Joey Trujillo-Bini <joeytrujillo-bini@MacBook-Pro-2.lan>
2023-06-23 18:41:46 -0400

    update intro content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,17 +2,18 @@ const About = () => {
     return (
         <div id="about" className="pb-16 m-4 lg:ml-8 pt-16 lg:pt-0" style={{color: "#888D91"}}>
             <p className="mb-2">
-                With a passion stemmed from the discovery of MySpace (🤣 remember that?), I am driven to develop intuitive
-                and user-friendly interfaces that consistently deliver exceptional user experiences.
+                With a passion stemming from my introduction to MySpace (🤣 sounds like eons ago) in 2008, I have been obsessed with 
+                software technologies and curious to understand how it all works. Customizing my own MySpace profile taught me the 
+                importance of having an engaging UI.
             </p>
             <p className="mb-2">
-                Over the course of my career, I have had the opportunity to work on a variety of projects,
-                ranging from simple websites to complex web applications, using the latest frontend technologies. 
+                Over the course of my career, I have had the opportunity to work on a variety of projects, ranging from simple 
+                websites to complex web applications, using the latest frontend technologies and applying what I've learned over 
+                the years. I place a strong emphasis on collaboration and teamwork, working seamlessly with all aspects of the team 
+                to deliver high-quality projects.
             </p>
             <p className="mb-2">
-                I place a strong emphasis on collaboration and teamwork and work seamlessly with all aspects of the
-                team to deliver high-quality projects. Ultimately, my goal is to create visually stunning and engaging
-                user interfaces that exceed both the expectations of clients and end-users.
+                When not working or engaging with family, I love to sleep. 
             </p>
         </div>
     )
